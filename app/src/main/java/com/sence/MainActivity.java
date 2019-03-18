fd42a43fbc3ef611afa7b2b3c5c6785d288e9ca8
yeqiu <1294054822@qq.com>
2019-03-18 12:15:41 +0800

    test

--- app/src/main/java/com/sence/MainActivity.java
+++ app/src/main/java/com/sence/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String name = "我的";
         main_home = findViewById(R.id.main_home);
         main_vip = findViewById(R.id.main_vip);
         main_kind = findViewById(R.id.main_kind);
