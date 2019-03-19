126d1e434255a5815f80ecb3b8ad8e4a1ba9535b
awy <731619934@qq.com>
2019-03-19 10:30:03 +0800

    Merge branch 'master' of https://github.com/android-super/Sence
    # Conflicts:
    #	app/src/main/java/com/sence/MainActivity.java

---
+++ app/src/main/java/com/sence/activity/EnjoyVipActivity.java
@@ -0,0 +1,15 @@
+package com.sence.activity;
+
+import android.os.Bundle;
+
+import com.sence.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EnjoyVipActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enjoyvip);
+    }
+}
