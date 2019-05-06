16118952162a9c0171f7cdceafe33c73064e5166
awy <731619934@qq.com>
2019-05-06 17:19:19 +0800

    Merge branch 'master' of https://github.com/android-super/Sence
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/sence/activity/ConfirmOrderActivity.java
    #	app/src/main/java/com/sence/net/HttpService.java
    #	app/src/main/java/com/sence/net/Urls.java

---
+++ app/src/main/java/com/sence/bean/response/PAbnormalAccountBean.java
@@ -0,0 +1,40 @@
+package com.sence.bean.response;
+
+public class PAbnormalAccountBean {
+    private String type;
+    private String msg;
+    private String device;
+    private String time;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getDevice() {
+        return device;
+    }
+
+    public void setDevice(String device) {
+        this.device = device;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
